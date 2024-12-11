82147808daf3d60e51852c54832acae957b8fc72
Andrii Chertkov <andriichertkov@gmail.com>
2024-12-11 23:35:51 +0200

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import '../global.css';
 
-import { Stack } from 'expo-router';
-
 export default function Layout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }} />
+      <StatusBar style="light" />
+    </GestureHandlerRootView>
+  );
 }
--- app/details.tsx
+++ app/details.tsx
@@ -1,17 +1,9 @@
-import { Stack, useLocalSearchParams } from 'expo-router';
-
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+import { Stack } from 'expo-router';
 
 export default function Details() {
-  const { name } = useLocalSearchParams();
-
   return (
     <>
       <Stack.Screen options={{ title: 'Details' }} />
-      <Container>
-        <ScreenContent path="screens/details.tsx" title={`Showing details for user ${name}`} />
-      </Container>
     </>
   );
 }
