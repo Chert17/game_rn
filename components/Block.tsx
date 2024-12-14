05e7a499272c24adb7351ac077eee11104212ed4
Andrii Chertkov <andriichertkov@gmail.com>
2024-12-14 16:48:07 +0200

    blocks life

--- components/Block.tsx
+++ components/Block.tsx
@@ -1,9 +1,26 @@
-import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import { useState } from 'react';
+import { Text } from 'react-native';
+import Animated, {
+  runOnJS,
+  useAnimatedReaction,
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
 
 import { useGameContext } from '~/providers/GameContext';
 
 export const Block = ({ index }: { index: number }) => {
   const { blocks } = useGameContext();
+  const [value, setValue] = useState(blocks?.value[index].val || 0);
+
+  useAnimatedReaction(
+    () => blocks?.value[index]?.val,
+    (val) => {
+      if (val !== undefined) {
+        runOnJS(setValue)(val);
+      }
+    }
+  );
 
   const style = useAnimatedStyle(() => {
     const block = blocks!.value[index];
@@ -20,8 +37,15 @@ export const Block = ({ index }: { index: number }) => {
       display: 'flex',
       backgroundColor: '#F5B52F',
       position: 'absolute',
+
+      alignItems: 'center',
+      justifyContent: 'center',
     };
   });
 
-  return <Animated.View style={style} />;
+  return (
+    <Animated.View style={style}>
+      <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 16 }}>{value}</Text>
+    </Animated.View>
+  );
 };
